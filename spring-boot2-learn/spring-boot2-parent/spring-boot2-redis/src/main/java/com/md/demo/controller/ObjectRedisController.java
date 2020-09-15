5267830983bbee3478816f095023c165655ce5eb
1134005157 <1134005157@qq.com>
2020-09-15 09:18:24 +0800

    mybatis

--- spring-boot2-learn/spring-boot2-parent/spring-boot2-redis/src/main/java/com/md/demo/controller/ObjectRedisController.java
+++ spring-boot2-learn/spring-boot2-parent/spring-boot2-redis/src/main/java/com/md/demo/controller/ObjectRedisController.java
@@ -2,18 +2,19 @@ package com.md.demo.controller;
 
 import javax.annotation.Resource;
 
-import com.alibaba.fastjson.JSON;
 import com.md.demo.util.RedisUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.md.demo.util.JsonResult;
 import com.md.demo.util.ResultCode;
 import com.md.demo.vo.Person;
+
+import java.util.List;
 
 @RestController
 public class ObjectRedisController {
@@ -22,15 +23,19 @@ public class ObjectRedisController {
 	@Resource
 	private RedisUtil redisUtil;
 
-	// 例如：http://localhost:9090/setObj?id=A001&name=minbo英文名&age=100
+    /**
+     * set object
+     */
 	@GetMapping("/setObj")
-	public JsonResult setObj(Person person) {
+	public JsonResult setObjToRedis(@RequestBody Person person) {
 		logger.debug("访问set：" + person.toString());
 		redisUtil.set("person:" + person.getId(), person);
 		return new JsonResult(ResultCode.SUCCESS);
 	}
 
-	// 例如：http://localhost:9090/getObj?id=A001
+    /**
+     * get object
+     */
 	@GetMapping("/getObj/{id}")
 	public JsonResult getObj(@PathVariable("id") String id) {
 		logger.debug("访问get:key id={}", id);
@@ -39,4 +44,24 @@ public class ObjectRedisController {
 		logger.debug("get obj result=" + result);
 		return new JsonResult(ResultCode.SUCCESS, person);
 	}
+
+    /**
+     * set list
+     */
+    @GetMapping("/setList")
+    public JsonResult setObjectList(@RequestBody List<Person> personList) {
+        logger.debug("访问set：" + personList.toString());
+        redisUtil.set("personList:", personList);
+        return new JsonResult(ResultCode.SUCCESS);
+    }
+
+    /**
+     * get list
+     */
+    @GetMapping("/getList")
+    public JsonResult getObjectList() {
+        List<Person> personList = (List<Person>) redisUtil.get("personList:");
+        logger.info(" get personList:" + personList);
+        return new JsonResult(ResultCode.SUCCESS, personList);
+    }
 }
