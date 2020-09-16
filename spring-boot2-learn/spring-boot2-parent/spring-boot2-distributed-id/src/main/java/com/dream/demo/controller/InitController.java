17f8520310971f99d1c76d34920f904d8b510324
1134005157 <1134005157@qq.com>
2020-09-16 11:30:21 +0800

    mybatis

--- spring-boot2-learn/spring-boot2-parent/spring-boot2-distributed-id/src/main/java/com/dream/demo/controller/InitController.java
+++ spring-boot2-learn/spring-boot2-parent/spring-boot2-distributed-id/src/main/java/com/dream/demo/controller/InitController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dream.core.leafid.MdIdsGen;
 
-
 @RestController
 public class InitController {
 
@@ -21,7 +20,6 @@ public class InitController {
 
     /**
      * 生成 id
-     * @return
      */
 	@GetMapping("/createId")
 	public BaseResponse getNewId() {
