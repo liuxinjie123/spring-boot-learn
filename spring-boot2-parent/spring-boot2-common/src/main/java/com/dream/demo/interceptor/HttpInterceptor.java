505dd3163e205693aa8d02efbe458dd78e9ed8c6
1134005157 <1134005157@qq.com>
2020-09-17 18:48:07 +0800

    mybatis

--- spring-boot2-parent/spring-boot2-common/src/main/java/com/dream/demo/interceptor/HttpInterceptor.java
+++ spring-boot2-parent/spring-boot2-common/src/main/java/com/dream/demo/interceptor/HttpInterceptor.java
@@ -1,5 +1,6 @@
 package com.dream.demo.interceptor;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -20,8 +23,10 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		String url = request.getRequestURL().toString();
+		String uri = request.getRequestURI();
 		String method = request.getMethod();
 		String queryString = "";
+
 		// 去掉最后一个空格
 		Map<String, String[]> params = request.getParameterMap();
 		for (String key : params.keySet()) {
@@ -33,8 +38,36 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
 		}
 		queryString = queryString.equals("") ? null : queryString.substring(0, queryString.length() - 1);
 		logger.info(String.format("请求参数, url: %s, method: %s, params: %s", url, method, queryString));
-		
-		return true;
+
+		// hello不做拦截
+		if (uri.equals("/hello")) {
+			return true;
+		}
+
+		//实现注入，调用服务层方法代码
+//		logger.info("【实现注入】调用服务层方法代码：demoService.sayHello()=" + this.demoService.sayHello());
+
+		// 其他拦截请求（请求必须都带上用户id）
+		String userId = request.getParameter("userId");
+		if (userId != null) {
+			return true;
+		} else {
+			Map<String, String> result = new HashMap<>();
+			result.put("code", "4001");
+			result.put("msg", "参数错误");
+			this.output(response, JSON.toJSONString(result));
+			return false;
+		}
+	}
+
+	/**
+	 * 输出结果
+	 */
+	private void output(HttpServletResponse response, String result) throws Exception {
+		response.setHeader("content-type", "text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(result);
 	}
 
 }
