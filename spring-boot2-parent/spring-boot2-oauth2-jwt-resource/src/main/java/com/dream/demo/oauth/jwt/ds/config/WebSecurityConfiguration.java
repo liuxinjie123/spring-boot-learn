27eaa7b1059af962c6e7533103f4139087bdd048
1134005157 <1134005157@qq.com>
2020-09-18 17:49:22 +0800

    mybatis

--- spring-boot2-parent/spring-boot2-oauth2-jwt-resource/src/main/java/com/dream/demo/oauth/jwt/ds/config/WebSecurityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.dream.demo.oauth.jwt.ds.config;
-
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration {
-
-}
